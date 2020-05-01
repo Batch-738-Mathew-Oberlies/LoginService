a614c9396ffc423e859404526477ae9874af8bdd
Gilbert Turner <grturner@5x5code.com>
2020-05-01 12:22:45 -0400

    UserService correction Corrected the annotation of the FeignClient being implemented by this service to correctly include the parameter for username.

--- src/main/java/com/revature/rideshare/services/UserService.java
+++ src/main/java/com/revature/rideshare/services/UserService.java
@@ -2,13 +2,13 @@ package com.revature.rideshare.services;
 
 import com.revature.rideshare.models.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @FeignClient(name = "user-service", url = "http://user-service:8080")
 public interface UserService {
-    @GetMapping
-    List<User> getUserByUsername(@SpringQueryMap String username);
+    @GetMapping(value = "/")
+    List<User> getUserByUsername(@RequestParam(name = "username") String username);
 }
